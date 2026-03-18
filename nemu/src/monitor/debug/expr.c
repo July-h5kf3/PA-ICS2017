c231f1a242953c8b74b1c6fb3bc74339655135ed
tracer-ics2017 <tracer@njuics.org>
2026-03-18 15:16:32 +0800

    >  compile 2312810 acd66 Linux DESKTOP-6N21GHG 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:16:32 up 40 min,  1 user,  load average: 0.00, 0.02, 0.00 aa9d61726c35ddd92491b93537c7f25bff41d7ca

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -146,7 +146,7 @@ uint32_t eval(int p,int q)
   }
   else if (p == q)
   {
-    printf("qwq");
+    // printf("qwq");
     return atoi(tokens[p].str);
   }
   else if(check_parentheses(p,q) == true)
@@ -160,6 +160,7 @@ uint32_t eval(int p,int q)
     int num_left = 0;
     for(int i = p;i <= q;i++)
     {
+      printf("%d th token is %c\n",i,tokens[i].type);
       if(num_left == 0 && (tokens[i].type == '+' || tokens[i].type == '-' || tokens[i].type == '*' || tokens[i].type == '/'))
       {
         dominant_op = &tokens[i];
