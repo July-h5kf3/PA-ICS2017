604bafa7ad3db5a5b4bfef53dd7bfd45b13b255a
tracer-ics2017 <tracer@njuics.org>
2026-03-24 21:01:26 +0800

    >  compile 2312810 acd66 Linux DESKTOP-6N21GHG 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:01:26 up 31 min,  1 user,  load average: 0.16, 0.09, 0.08 4fbf1cf2e2a28dec61a8dfe3800f4ef948188e78

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -38,7 +38,9 @@ static inline make_DopHelper(SI) {
    *
    op->simm = ???
    */
-  TODO();
+  if(op->width == 1)op->simm = (int8_t)instr_fetch(eip,op->width);
+  else op->simm = (int64_t)instr_fetch(eip,op->width);
+  // TODO();
 
   rtl_li(&op->val, op->simm);
 
