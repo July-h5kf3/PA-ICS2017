2844ce7bbcc7f2b21925e49c83ca877804451569
tracer-ics2017 <tracer@njuics.org>
2026-04-08 14:21:10 +0800

    >  compile 2312810 acd66 Linux DESKTOP-6N21GHG 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  14:21:10 up 59 min,  1 user,  load average: 0.08, 0.23, 0.17 8cb06b97ce8cc86c1904b6cb8bfef36430e4f023

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -47,6 +47,7 @@ void pio_write(ioaddr_t, int, uint32_t);
 make_EHelper(in) {
   // TODO();
   id_dest->val = pio_read(id_src->val, id_dest->width);
+  operand_write(id_dest, &id_dest->val);
 
   print_asm_template2(in);
 
