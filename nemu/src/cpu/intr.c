5efb3cc15136842ca37078a796fbfbbcba3b84f3
tracer-ics2017 <tracer@njuics.org>
2026-04-22 15:26:42 +0800

    >  compile 2312810 acd66 Linux DESKTOP-6N21GHG 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:26:42 up  2:40,  1 user,  load average: 0.06, 0.07, 0.03 b694cb1ec138cf301eb1a50f2343014b5c20eb39

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,9 +5,26 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+  assert(NO * 8 + 7 <= cpu.idtr.limit);
 
-  TODO();
+  vaddr_t gate_addr = cpu.idtr.base + NO * 8;
+  uint32_t gate_lo = vaddr_read(gate_addr, 4);
+  uint32_t gate_hi = vaddr_read(gate_addr + 4, 4);
+  vaddr_t intr_addr = (gate_lo & 0xffff) | (gate_hi & 0xffff0000);
+
+  rtl_push(&cpu.eflags, 4);
+
+  rtlreg_t cs = 0x8;
+  rtl_push(&cs, 4);
+  rtl_push(&ret_addr, 4);
+
+  decoding.is_jmp = 1;
+  decoding.jmp_eip = intr_addr;
 }
 
 void dev_raise_intr() {
+  const uint8_t IRQ_TIMER = 32;
+  if (cpu.Eflags.IF) {
+    raise_intr(IRQ_TIMER, cpu.eip);
+  }
 }
