6e8f8733fd1065875dfa05f766881c33c671f568
tracer-ics2017 <tracer@njuics.org>
2026-04-22 17:03:16 +0800

    >  run 2312810 acd66 Linux DESKTOP-6N21GHG 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:03:16 up  4:16,  1 user,  load average: 0.13, 0.08, 0.07 fd167820c2ca6e9a9b7122e63f97a1bfa926e2a

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,11 +4,15 @@
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
 
   switch (a[0]) {
     case SYS_none:
       r->eax = 1;
       return r;
+    case SYS_exit:
+      _halt(a[1]);
+      return NULL;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
