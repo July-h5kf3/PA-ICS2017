b097091d68e3029c8d2c1eff342bb0bc55561d4b
tracer-ics2017 <tracer@njuics.org>
2026-05-09 17:24:47 +0800

    >  run 2312810 acd66 Linux DESKTOP-6N21GHG 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:24:47 up  1:11,  1 user,  load average: 0.88, 0.43, 0.24 734280d72e4fa0e2ce6156faaa1793ed8442897c

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -10,19 +10,30 @@ static const char *keyname[256] __attribute__((used)) = {
 
 size_t events_read(void *buf, size_t len) {
   static unsigned long last_time = 0;
+  char event[64];
 
   while (1) {
     int key = _read_key();
     if (key != _KEY_NONE) {
       bool keydown = (key & 0x8000) != 0;
       int code = key & ~0x8000;
-      return snprintf(buf, len, "%s %s\n", keydown ? "kd" : "ku", keyname[code]);
+      int n = snprintf(event, sizeof(event), "%s %s\n", keydown ? "kd" : "ku", keyname[code]);
+      if ((size_t)n > len) {
+        n = len;
+      }
+      memcpy(buf, event, n);
+      return n;
     }
 
     unsigned long now = _uptime();
     if (now - last_time >= 1000 / 30) {
       last_time = now;
-      return snprintf(buf, len, "t %lu\n", now);
+      int n = snprintf(event, sizeof(event), "t %u\n", (unsigned int)now);
+      if ((size_t)n > len) {
+        n = len;
+      }
+      memcpy(buf, event, n);
+      return n;
     }
   }
 }
