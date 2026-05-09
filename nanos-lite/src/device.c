9ecbc5520a187bbedb7c4e213a3283b83402e887
tracer-ics2017 <tracer@njuics.org>
2026-05-09 17:18:53 +0800

    >  run 2312810 acd66 Linux DESKTOP-6N21GHG 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:18:53 up  1:05,  1 user,  load average: 0.09, 0.18, 0.13 3fe257543ccc8701d26f9a558ca61a4991a682c1

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -10,21 +10,21 @@ static const char *keyname[256] __attribute__((used)) = {
 
 size_t events_read(void *buf, size_t len) {
   static unsigned long last_time = 0;
-  int key = _read_key();
 
-  if (key != _KEY_NONE) {
-    bool keydown = (key & 0x8000) != 0;
-    int code = key & ~0x8000;
-    return snprintf(buf, len, "%s %s\n", keydown ? "kd" : "ku", keyname[code]);
+  while (1) {
+    int key = _read_key();
+    if (key != _KEY_NONE) {
+      bool keydown = (key & 0x8000) != 0;
+      int code = key & ~0x8000;
+      return snprintf(buf, len, "%s %s\n", keydown ? "kd" : "ku", keyname[code]);
+    }
+
+    unsigned long now = _uptime();
+    if (now - last_time >= 1000 / 30) {
+      last_time = now;
+      return snprintf(buf, len, "t %lu\n", now);
+    }
   }
-
-  unsigned long now = _uptime();
-  if (now != last_time) {
-    last_time = now;
-    return snprintf(buf, len, "t %lu\n", now);
-  }
-
-  return 0;
 }
 
 static char dispinfo[128] __attribute__((used));
