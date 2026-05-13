fc08aa32c05418169dbafd8ac76030c714c9f8b6
tracer-ics2017 <tracer@njuics.org>
2026-05-13 22:16:46 +0800

    >  run 2312810 acd66 Linux DESKTOP-6N21GHG 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:16:46 up  6:58,  1 user,  load average: 0.22, 0.11, 0.07 5e0561bf4e7f7e8a1955f7e37efc3fab60144093

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -31,8 +31,10 @@ void load_prog(const char *filename) {
 }
 
 _RegSet* schedule(_RegSet *prev) {
-  (void)prev;
-  assert(current != NULL);
+  if (current != NULL) {
+    current->tf = prev;
+  }
+  current = &pcb[0];
   _switch(&current->as);
   return current->tf;
 }
